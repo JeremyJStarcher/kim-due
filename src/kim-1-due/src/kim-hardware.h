2786abce3fd3468fdc7cf725d310d2d909a151fa
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-05-12 17:19:07 -0400

    Split some code apart for clarity.

---
+++ src/kim-1-due/src/kim-hardware.h
@@ -0,0 +1,14 @@
+#ifndef KIM_HARDWARE_H
+#define KIM_HARDARE_H
+
+#include <Arduino.h>
+
+void setupUno(void);
+int freeRam(void);
+void interpretkeys(void);
+uint8_t xkeyPressed(void);
+
+extern uint8_t curkey;
+
+#endif
+
