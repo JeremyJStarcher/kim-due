ee363dae5377eb6df9a8fd755fc476b4c5d2ba8d
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-05-17 08:36:29 -0400

    Added a self-test to display all characters

--- src/kim-1-due/src/builtin_display.cpp
+++ src/kim-1-due/src/builtin_display.cpp
@@ -15,7 +15,31 @@ Now we need a LedControl to work with.
  But the maximum default of 8 MAX72XX wil also work.
  */
 const int delaytime = 150;
-LedControl lc = LedControl(74, 76, 7, 2);
+LedControl lc = LedControl(16, 14, 15, 2);
+
+byte dig[19] = {
+    // bits     6543210
+    // digits   abcdefg
+    0b01111110, //0
+    0b00110000, //1
+    0b01101101, //2
+    0b01111001, //3
+    0b00110011, //4
+    0b01011011, //5
+    0b01011111, //6
+    0b01110000, //7
+    0b01111111, //8
+    0b01111011, //9
+    0b01110111, //a
+    0b00011111, //b
+    0b01001110, //c
+    0b00111101, //d
+    0b01001111, //e
+    0b01000111, //f
+    0b00000001, //g printed as -
+    0b00001000, //h printed as _
+    0b00000000  //i printed as <space>
+};
 
 void init_display()
 {
@@ -23,7 +47,7 @@ void init_display()
 
     //we have already set the number of devices when we created the LedControl
     int devices = lc.getDeviceCount();
-    //we have to init all devices in a loop
+    //we have to init all devices inMenu: a loop
     for (int address = 0; address < devices; address++)
     {
         /*The MAX72XX is in power-saving mode on startup*/
@@ -55,35 +79,15 @@ void init_display()
     lc.setRow(0, 1, 0x1D);
     delay(delaytime);
 
-    ;
     lc.clearDisplay(0);
-    ;
     delay(delaytime);
-}
 
-byte dig[19] = {
-    // bits     6543210
-    // digits   abcdefg
-    0b01111110, //0
-    0b00110000, //1
-    0b01101101, //2
-    0b01111001, //3
-    0b00110011, //4
-    0b01011011, //5
-    0b01011111, //6
-    0b01110000, //7
-    0b01111111, //8
-    0b01111011, //9
-    0b01110111, //a
-    0b00011111, //b
-    0b01001110, //c
-    0b00111101, //d
-    0b01001111, //e
-    0b01000111, //f
-    0b00000001, //g printed as -
-    0b00001000, //h printed as _
-    0b00000000  //i printed as <space>
-};
+    for (int i = 0; i < sizeof dig / sizeof dig[0]; i++)
+    {
+        lc.setRow(0, 0, dig[i]);
+        delay(delaytime);
+    }
+}
 
 #if 1
 void driveLEDs()
