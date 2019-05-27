bc410b420d968487d3c4dc33207447d74265b0d4
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-05-27 15:33:28 -0400

    Added debugging tool

--- src/kim-1-due/src/cpu.cpp
+++ src/kim-1-due/src/cpu.cpp
@@ -17,6 +17,15 @@
 #include "boardhardware.h"
 #include "builtin_display.h"
 
+void getBin(int num, char *str)
+{
+    *(str + 5) = '\0';
+    int mask = 0x10 << 1;
+    while (mask >>= 1) {
+        *str++ = !!(mask & num) + '0';
+    }
+}
+
 #ifdef USE_EPROM
 extern uint8_t eepromread(uint16_t eepromaddress);
 extern void eepromwrite(uint16_t eepromaddress, uint8_t bytevalue);
