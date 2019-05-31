1c47d311bf725e05281e0940d1f1102d65d110f6
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-05-31 01:13:03 -0400

    Added ASCII art of KIM keypad details

---
+++ src/kim-1-due/src/keypad.cpp
@@ -0,0 +1,13 @@
+/*
+    ioPAD          (opPDB .....XX.)
+     bit         00    01    10    11        ioPAD
+  assignment    row0  row1  row2  row3       value
+  -----------   ----- ----  ----  ----    ---------
+      bit6        0    7     E            1011 1111
+      bit5        1    8     F            1101 1111
+      bit4        2    9     AD           1110 1111
+      bit3        3    A     DA           1111 0111
+      bit2        4    B     +            1111 1011
+      bit1        5    C     GO           1111 1101
+      bit0        6    D     PC   SST     1111 1110
+ */
