d4325de3d1fd14ed232ceeead72725b17c80581c
Jeremy J Starcher <jeremy.starcher@gmail.com>
2019-06-05 02:35:31 -0400

    Cleaned up build

--- browser/build.sh
+++ browser/build.sh
@@ -1,19 +1,33 @@
 # emcc dice-roll.c ../../src/kim-1-due/src/webmain.cpp -s LINKABLE=1 -s EXPORT_ALL=1 -s WASM=1 -O3 -D TARGETWEB -o index.js  
 
 # Make a local copy so we can access it via our source maps
-rm -rf csrc
-cp ../src/kim-1-due/src csrc -Rv
+DIST="dist/";
+rm -rf ${DIST}
+mkdir -p ${DIST}
+
+CSRC="${DIST}csrc"
+cp ../src/kim-1-due/src ${CSRC} -Rv
 
 SOURCE="dice-roll.c"
-SOURCE="${SOURCE} ./csrc/webmain.cpp"
-SOURCE="${SOURCE} ./csrc/cpu.cpp"
-SOURCE="${SOURCE} ./csrc/builtin_display.cpp"
-SOURCE="${SOURCE} ./csrc/kim-hardware.cpp"
+SOURCE="${SOURCE} ${CSRC}/webmain.cpp"
+SOURCE="${SOURCE} ${CSRC}/cpu.cpp"
+SOURCE="${SOURCE} ${CSRC}/builtin_display.cpp"
+SOURCE="${SOURCE} ${CSRC}/kim-hardware.cpp"
+
+echo "SOURCE: ${SOURCE}"
+echo "CSRC: ${CSRC}"
 
 LEVEL="-O0 -g4"
 #LEVEL="-O3"
 
-emcc ${SOURCE} -s EXPORTED_FUNCTIONS='["_websetup", "_webloop", "_injectkey"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -s WASM=1 ${LEVEL} -D TARGETWEB -o index.js
+emcc ${SOURCE} -s EXPORTED_FUNCTIONS='["_websetup", "_webloop", "_injectkey"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -s WASM=1 ${LEVEL} -D TARGETWEB -o ${DIST}/index.js
 
-emrun --no_browser --port 8080 .
+cp index.html ${DIST}
+cp *.css ${DIST}
+cp *.js ${DIST}
+
+cd ${DIST}
+emrun --no_browser --port 8080 ${DIST} 
+cd ..
+
 #python -m SimpleHTTPServer 9000
