48bb2e318fdb02987705d7564ab21ee0b54f3bda
Jeremy J Starcher <Jeremy.Starcher@gmail.com>
2019-06-14 02:25:21 -0400

    Runs in WASM

--- src/kim-1-due/src/MemIo/MemIoRom.cpp
+++ src/kim-1-due/src/MemIo/MemIoRom.cpp
@@ -1,13 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Arduino.h>
-
-#ifdef TARGETWEB
-#include "fake-progmen.h"
-#include <emscripten/emscripten.h>
-#else
-#include <avr/pgmspace.h>
-#endif
 
 #include "MemIoRom.h"
 uint8_t MemIoRom::read(uint16_t address)
@@ -16,12 +8,6 @@ uint8_t MemIoRom::read(uint16_t address)
 
 #ifdef __AVR__
     uint8_t ret = (pgm_read_byte_near(this->data + offset));
-#if 0
-    Serial.print(" ");
-    Serial.print(ret, HEX);
-    Serial.print(" ");
-    delay(1500);
-#endif
     return ret;
 #else
     return this->data[offset];
